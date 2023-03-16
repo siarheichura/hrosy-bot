f6804245220854bf9cb8bb6bad76d90ccccf20ca
Sereja Chura <chura.siarhei@gmail.com>
2023-03-17 00:22:15 +0400

    test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use(express.static("./dist/hrosy-bot-ng"));
+app.use(express.static('./dist/hrosy-bot-ng'))
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", {root: "dist/hrosy-bot-ng"});
-});
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/hrosy-bot-ng' })
+})
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
